ef2c58658efd6ce4d7e1ee76786e2d55c35caca5
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-06-05 15:05:14 -0400

    add counter screen

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello Flutter',
+        ),
+      ),
+    );
+  }
+}
